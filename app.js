80f532317374c70e335903ceec2e4a841ac365e4
fathulqorib <adie.share@gmail.com>
2018-08-31 10:56:20 +0700

    session

--- app.js
+++ app.js
@@ -1,13 +1,48 @@
 const express = require('express')
 const Routes = require('./routes')
 const path = require('path')
-
 const app = express()
+const bcrypt = require('bcrypt')
+// const morgan = require('morgan');
+
+const authentication = require("./helper/authentication.js");
+
+var session = require('express-session')
+var sess = {
+    key: 'user_sid',
+    secret: 'somerandonstuffs',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
+}
 
 app.set('view engine', 'ejs')
+// app.use(morgan('dev'))
+app.use(session(sess));
+
+app.get("/",function(req, res){
+    let name = "dimas";
+    req.session.user = {
+        name:"Dimas",
+        email:"alhusna901@gmail.com"
+    };
+    res.send(" session name sudah di set ");
+     //console.log( req.session );
+});
+
+app.get("/dashboard",authentication,function(req,res){
+    //res.send(" session name : "+ req.session.name );
+    console.log("dashboard");
+    console.log( req.session.user );
+});
+
+app.get("/login",function(req, res){
+    console.log(" ini login ");
+})
 
 app.use(express.urlencoded({ extended : false }))
 app.use(express.static(path.join(__dirname + '/public')))
 app.use('/', Routes)
-
 app.listen(3000, () => console.log('app listen in port 3000'))
--- models/user.js
+++ models/user.js
@@ -4,10 +4,36 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     age: DataTypes.INTEGER,
     gender: DataTypes.STRING,
-    email: DataTypes.STRING,
-    username: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      unique: true,
+      allowNull: false
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  
+  }, {
+
+    hooks: {
+      beforeCreate: (user) => {
+        const salt = bcrypt.genSaltSync();
+        user.password = bcrypt.hashSync(user.password, salt);
+      }
+    },
+    instanceMethods: {
+      validPassword: function(password) {
+        return bcrypt.compareSync(password, this.password);
+      }
+    }
+
+  });
   User.associate = function(models) {
     
     User.belongsToMany(models.Trip, {
